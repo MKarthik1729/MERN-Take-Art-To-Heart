763de6b6206c2e0085a8f5953e190abad632c294
Mkarthik1729 <mkarthik1729@gmail.com>
2022-10-05 10:20:01 +0530

    login done

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { UserContext } from '../context/Context'
 
 function Login() {
     const navigate = useNavigate()
+    const {name,setName} = useContext(UserContext)
     const HandleSubmit = (e) =>{
         e.preventDefault()
+        console.log(name)
+        setName('karthik')
         console.log('data done working fine')
         navigate('/user')
     }
@@ -12,8 +16,8 @@ function Login() {
   return (
     <div>
         <form onSubmit={HandleSubmit}>
-            <input type='email' placeholder='Email' />
-            <input type='password' placeholder='Password' />
+            <input type='email' placeholder='Email' /><br/>
+            <input type='password' placeholder='Password' /><br/>
             <button type='submit'>Submit</button>
         </form>
     </div>
---
+++ server/database/userRegisterModel.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+let userRegister = new Schema({
+	username: {
+		type: String,
+		required: true,
+	},
+	useremail: {
+		type: String,
+		required: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	country: {
+		type: String,
+		required: true,
+	},
+	dob: {
+		type: String,
+		required: true,
+	},
+	gender: {
+		type: String,
+		required: true,
+	},
+	usertype: {
+		type: String,
+		required: true,
+	},
+});
+
+module.exports = mongoose.model("userdata",userRegister);
